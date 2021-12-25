a5ee591670ed1c04a7092a0ac68ada7ef63b382d
woodonggyu <woodonggyu92@gmail.com>
2021-12-26 01:56:39 +0900

    feat: Manages the account alias, password policy    + added. https://docs.aws.amazon.com/ko_kr/IAM/latest/UserGuide/console_account-alias.html    + added. https://docs.aws.amazon.com/ko_kr/IAM/latest/UserGuide/id_credentials_passwords_account-policy.html

---
+++ examples/iam-account/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "default"
+}
+
+module "iam_account" {
+  source = "../../modules/iam-account"
+
+  account_alias = "terraform-alias"
+
+  allow_users_to_change_password = true
+  hard_expiry                    = false
+  max_password_age               = 90
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  require_uppercase_characters   = true
+}
---
+++ modules/iam-account/variables.tf
@@ -0,0 +1,65 @@
+variable "account_alias" {
+  type        = string
+  description = "The account alias"
+  default     = null
+}
+
+variable "create_account_password_policy" {
+  type        = bool
+  description = "Whether to create AWS IAM account password policy"
+  default     = true
+}
+
+variable "allow_users_to_change_password" {
+  type        = bool
+  description = "Whether to allow users to change their own password"
+  default     = true
+}
+
+variable "hard_expiry" {
+  type        = bool
+  description = "Whether users are prevented from setting a new password after their password has expired"
+  default     = false
+}
+
+variable "max_password_age" {
+  type        = number
+  description = "The number of days that an user password is valid."
+  default     = 90
+}
+
+variable "minimum_password_length" {
+  type        = number
+  description = "Minimum length to require for user passwords."
+  default     = 8
+}
+
+variable "password_reuse_prevention" {
+  type        = number
+  description = "The number of previous passwords that users are prevented from reusing."
+  default     = null
+}
+
+variable "require_lowercase_characters" {
+  type        = bool
+  description = "Whether to require lowercase characters for user passwords."
+  default     = true
+}
+
+variable "require_numbers" {
+  type        = bool
+  description = "Whether to require numbers for user passwords."
+  default     = true
+}
+
+variable "require_symbols" {
+  type        = bool
+  description = "Whether to require symbols for user passwords."
+  default     = true
+}
+
+variable "require_uppercase_characters" {
+  type        = bool
+  description = "Whether to require uppercase characters for user passwords."
+  default     = true
+}
